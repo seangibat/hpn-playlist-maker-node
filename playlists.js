1b7830f334a29a1d7d6eb89ead4c9a4037c0495e
Sean Gibat <sgibat@gmail.com>
2015-05-27 19:33:57 -0700

    finished youtube adding library

--- playlists.js
+++ playlists.js
@@ -1,103 +1,128 @@
 var request = require('request');
-var cheerio = require('cheerio');
 var google = require('googleapis');
-var youtube = google.youtube('v3');
-var storage = require('node-persist');
-var envVars = require('./secrets.js'); // options
+var Promise = require('bluebird');
+var envVars = require('./secrets.js');
+var scraper = require('./scraper');
 var OAuth2 = google.auth.OAuth2;
+
 var oauth2Client = new OAuth2(envVars.CLIENT_ID, envVars.CLIENT_SECRET, envVars.REDIRECT_URL);
 google.options({ auth: oauth2Client });
 
+var youtube = google.youtube('v3');
 
-var getNewAuthToken = function(cb){
-  url = 'https://accounts.google.com/o/oauth2/token';
-  payload = { 
-    grant_type:'refresh_token', 
-    client_id: envVars.CLIENT_ID, 
-    client_secret: envVars.CLIENT_SECRET, 
-    refresh_token: envVars.REFRESH_TOKEN
-  };
-  headers = {"Content-Type": "application/x-www-form-urlencoded"};
-  
-  request.post({
-    uri: url,
-    form: payload,
-    headers: headers
-  }, function(error, response, body){
-    envVars.ACCESS_TOKEN = JSON.parse(body).access_token;
-    oauth2Client.setCredentials({
-      access_token: envVars.ACCESS_TOKEN,
+Promise.promisifyAll(youtube.playlists);
+Promise.promisifyAll(youtube.playlistItems);
+
+var getNewAuthToken = function(){
+  var options = {
+    uri: "https://accounts.google.com/o/oauth2/token",
+    form: { 
+      grant_type:'refresh_token', 
+      client_id: envVars.CLIENT_ID, 
+      client_secret: envVars.CLIENT_SECRET, 
       refresh_token: envVars.REFRESH_TOKEN
-    });
-    cb();
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  };
+  
+  return new Promise(function(resolve, reject){
+    request.post(options, function(err, response, body){
+      if (err) reject(err);
+      envVars.ACCESS_TOKEN = JSON.parse(body).access_token;
+      oauth2Client.setCredentials({
+        access_token: envVars.ACCESS_TOKEN,
+        refresh_token: envVars.REFRESH_TOKEN
+      });
+      resolve();
+    }); 
   });
 };
 
 // cb is first because the other two params are optional
-var listPlaylists = function(cb, arr, nextPageToken){ 
-  youtube.playlists.list({
+var listPlaylists = function(prevPage){ 
+  var options = {
     maxResults: 50,
     mine: true,
     part: "snippet",
-    pageToken: nextPageToken || ""
-  }, function(err, data){
-    arr = (arr) ? arr.concat(data.items) : data.items;
-    if (data.nextPageToken){
-      listPlaylists(cb, arr, data.nextPageToken);
-    }
-    else {
-      cb(arr);
-    }
-  });
+    pageToken: prevPage ? prevPage.nextPageToken : ""
+  };
+
+  return youtube.playlists.listAsync(options)
+    .then(function(data){
+      data = data[0];
+      data.items = prevPage ? prevPage.items.concat(data.items) : data.items;
+      if (data.nextPageToken) { return listPlaylists(data); }
+      else { return data.items; }
+    });
 };
 
-var createPlaylist = function(title, description, cb){
-  youtube.playlists.insert({
+var createPlaylist = function(thread){
+  var options = {
     part: "snippet,status",
     resource: {
       snippet: { 
-        title: title.replace(/[^a-z0-9\s]/gmi, ""), 
-        description: description 
+        title: thread.title.replace(/[^a-z0-9\s]/gmi, ""), 
+        description: thread.description 
       }, 
       status: {
         privacyStatus: 'public'
       } 
     }
-  }, cb);
+  };
+  return youtube.playlists.insertAsync(options)
+    .then(function(response){
+      thread.playlistId = response[1].body.id;
+      return thread;
+    });
 };
 
-var addToPlaylist = function(playlistId, youtubeId, cb){
-  youtube.playlistItems.insert({
+var addToPlaylist = function(youtube){
+  var params = {
     part: 'snippet',
     resource: { 
       'snippet': { 
-        'playlistId': playlistId, 
+        'playlistId': youtube.playlistId, 
         'resourceId': { 
-          'kind':'youtube#video', 'videoId':youtubeId 
+          'kind':'youtube#video', 
+          'videoId': youtube.videoId
         } 
       } 
     }
-  }, cb);
+  };
+
+  return youtube.playlistItems.insertAsync(params);
 };
 
-var playlist = function(req, res){
-  // var threadId = req.params.threadId,
-  //     forumId  = req.params.forumId, $;
+var findOrCreatePlaylist = function(thread){
+  return listPlaylists()
+    .then(function(playlists){
+      playlists.some(function(playlist){
+        if (playlist.snippet.description === thread.description) {
+          thread.playlistId = playlist.id;
+          return true;
+        }
+        return false;
+      });
 
-  var threadId = "55097";
-  var forumId = "1";
-
-  createPlaylist("test", "wfwf", function(err, data){
-    console.log(err, data);
-    addToPlaylist(data.id, "y_2VAEeeMVo", function(err, data){
-      console.log(err, data);
+      if (!thread.playlistId) return createPlaylist(thread);
+      else return thread;
     });
-  });
 };
 
+var addAllToPlaylist = function(thread){
+  return Promise.all(thread.youtubeIds.map(function(youtubeId){
+    return addToPlaylist({
+      videoId: youtubeId,
+      playlistId: playlistId
+    });
+  }));
+};
 
-getNewAuthToken(function(){
-  playlist();
-});
+getNewAuthToken()
+.then(listPlaylists)
+.then(findOrCreatePlaylist)
+.then(addAllToPlaylist);
 
-exports.process = playlist;
+// exports.process = playlist;
--- scraper.js
+++ scraper.js
@@ -1,25 +1,26 @@
 var cheerio = require('cheerio');
-var request  = require('request');
+var request = require('request');
 var _ = require('underscore');
 
-var getNumPages = function(id){
-  return getThreadPage(id).then(function(page){
+var getNumPagesAndTitle = function(id) {
+  return getThreadPage(id).then(function(page) {
     var $ = cheerio.load(page);
     var numPages = $('.topic-actions .pagination span a').last().text();
+    var title = $('#page-body h2 a').text().replace(/[<>\/\\]/g,"");
     numPages = numPages ? parseInt(numPages) : 1;
-    return numPages;
+    return {title: title, numPages: numPages};
   });
 };
 
-var numPagesToPageNumbers = function(numPages){
+var numPagesToPageNumbers = function(numPages) {
   return _.range(1, numPages + 1);
 };
 
-var getThreadPage = function(id, page){
-  return new Promise(function(resolve, reject){
+var getThreadPage = function(id, page) {
+  return new Promise(function(resolve, reject) {
     page = page || 1;
     page = (page - 1) * 30;
-    request('http://forums.hipinion.com/viewtopic.php?f=1&t=' + id + '&start=' + page, function(error, response, body){
+    request('http://forums.hipinion.com/viewtopic.php?f=1&t=' + id + '&start=' + page, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         resolve(body);
       } else {
@@ -29,28 +30,40 @@ var getThreadPage = function(id, page){
   });
 };
 
-var threadPageToYoutubeIds = function(page){
+var threadPageToYoutubeIds = function(page) {
   var $ = cheerio.load(page);
-  var urls = $('embed').map(function(i, d){ 
+  var urls = $('embed').map(function(i, d) {
     var url = $(d).attr('src');
-    return url.split('youtube.com/v/')[1].substring(0,11)
+    return url.split('youtube.com/v/')[1].substring(0, 11);
   });
   return urls.get();
 };
 
-var getWholeThread = function(id, numPages){
-  return getNumPages(id)
+var getWholeThread = function(id) {
+  var title;
+
+  return getNumPagesAndTitle(id)
+    .then(function(thread){
+      title = thread.title;
+      return thread.numPages;
+    })
     .then(numPagesToPageNumbers)
-    .then(function(pageNumbers){
+    .then(function(pageNumbers) {
       return pageNumbers.map(getThreadPage.bind(null, id))
     })
-    .then(function(requests){
+    .then(function(requests) {
       return Promise.all(requests);
     })
-    .then(function(pagesArray){
+    .then(function(pagesArray) {
       return pagesArray.map(threadPageToYoutubeIds)
     })
-    .then(_.flatten);
+    .then(function(youtubeIds){
+      return {
+        youtubeIds: _.flatten(youtubeIds),
+        description: id,
+        title: title
+      };
+    });
 };
 
 module.exports = getWholeThread;
